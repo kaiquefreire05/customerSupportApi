662361a162ad2b813d3b47207c4c97c55e134a87
Kaíque Freire dos Santos <kaiquefreiresantos05@gmail.com>
2025-01-29 21:13:34 -0300

    alter the return of method

--- src/main/java/com/api/customersupport/application/ports/output/TicketRepositoryPort.java
+++ src/main/java/com/api/customersupport/application/ports/output/TicketRepositoryPort.java
@@ -1,16 +1,17 @@
 package com.api.customersupport.application.ports.output;
 
+import com.api.customersupport.domain.exceptions.TicketSupportNotFoundException;
 import com.api.customersupport.domain.models.SupportTicket;
 
 import java.util.List;
 import java.util.UUID;
 
 public interface TicketRepositoryPort {
-    void assignTicketToAgent(Long ticketId, UUID agentId);
+    Boolean assignTicketToAgent(Long ticketId, UUID agentId);
     Boolean closeTicket(Long ticketId);
     SupportTicket createTicket(SupportTicket ticket);
     Boolean deleteTicket(Long ticketId);
-    SupportTicket getTicketById(Long ticketId);
+    SupportTicket getTicketById(Long ticketId) throws TicketSupportNotFoundException;
     List<SupportTicket> listTicketsByAgent(UUID agentId);
     List<SupportTicket> listOpenTickets();
     List<SupportTicket> listOpenTicketsWithoutAgent();
