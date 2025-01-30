8eff72803c0fcb1b74a52eef07a0d3bf6ebf7b06
Kaíque Freire dos Santos <kaiquefreiresantos05@gmail.com>
2025-01-30 19:54:55 -0300

    adding exception in hierarchy

--- src/main/java/com/api/customersupport/application/ports/output/FeedbackRepositoryPort.java
+++ src/main/java/com/api/customersupport/application/ports/output/FeedbackRepositoryPort.java
@@ -1,13 +1,14 @@
 package com.api.customersupport.application.ports.output;
 
+import com.api.customersupport.domain.exceptions.FeedbackNotFoundException;
 import com.api.customersupport.domain.models.Feedback;
 
 import java.util.List;
 
 public interface FeedbackRepositoryPort {
     Boolean deleteFeedback(Long id);
-    Feedback findFeedbackById(Long id);
-    Feedback findFeedbackByTicketId(Long ticketId);
+    Feedback findFeedbackById(Long id) throws FeedbackNotFoundException;
+    Feedback findFeedbackByTicketId(Long ticketId) throws FeedbackNotFoundException;
     Boolean provideFeedback(Feedback feedback);
     Feedback updateFeedback(Feedback feedback);
     List<Feedback> listFeedbacks();
